8b3c2d375ecac218002bc17bc0300c5c54273924
Dax Garner <dax.garner@gmail.com>
2016-10-26 18:02:30 -0500

    Adds field printing functionality

--- mineclear_eval.py
+++ mineclear_eval.py
@@ -14,6 +14,7 @@ import string
 class Game:
     def __init__(self):
         """ Initializes game with nothing to do """
+        self.setup_valid = False
         self.actions = []
         self.move = {'north': -1, 'south': 1, 'east': 1, 'west': -1}
         self.shoot = {'alpha':[(-1, -1), (-1,  1), (1, -1), (1, 1)],
@@ -21,10 +22,13 @@ class Game:
                       'gamma':[(-1,  0), ( 0,  0), (1,  0)],
                       'delta':[( 0, -1), ( 0,  0), (0,  1)]
                      }
-        self.field = [] # This is an absolute field
+        self.field = [] # This is a relative field, that maintains the ship at the center
         self.field_size = {'x': 0, 'y': 0}
+        self.ship = (0, 0)
         self.lowercase_offset = -96 # a = 97 = 1 km
         self.uppercase_offset = -38 # A = 65 = 27 km
+        self.success = False
+        self.score = 0
     def setup(self, ffile, sfile):
         """ Parses the files and initialies the action list and field """
         # Parse field file and evalute depth
@@ -32,20 +36,25 @@ class Game:
             field_lines = [l.rstrip('\n') for l in ff.readlines()] # Get without newline characters
             i = 0
             for l in field_lines:
-                self.field.append([])
                 j = 0
+                self.field.append([])
                 for c in l:
                     # Detect mine
                     if (c in string.ascii_lowercase) or (c in string.ascii_uppercase):
+                        # Add to field
                         self.field[i].append(self.char_to_depth(c))
+                        # Update starting score
+                        self.score = self.score + 10
                     else: # No mine detected
                         if c != '.':
                             print "Warning: Character " + c + " implies no mine in field at: (" + str(i) + ", " + str(j) + ")"
-                        self.field[i].append(None)
+                        self.field[j].append(None)
                     j = j + 1
                 self.field_size['x'] = j if (j > self.field_size['x']) else self.field_size['x']
                 i = i + 1
             self.field_size['y'] = i
+        # Set ship location
+        self.ship = (i/2, j/2)
         # Parse script action list
         with open(sfile) as sf:
             action_lines = sf.readlines()
@@ -99,7 +108,66 @@ class Game:
                     print "Error: Unknown action: " + s[0]
             else:
                 continue
+        self.setup_valid = True
         return True
+
+    def play(self):
+        """ Cyclical operations of the game after a valid setup """
+        if not self.setup_valid:
+            print "Error: Game was not properly setup"
+            return "fail", 0
+        i = 1
+        while(not self.end()):
+            print "Step " + str(i) + '\n'
+            self.print_field()
+            self.step()
+            self.print_field()
+            i = i + 1
+            sys.exit()
+        result = "pass" if self.success else "fail"
+        return result, self.score
+
+    def end(self):
+        """ Checks for all end conditions """
+        end_game = False
+        # 1 - passed mine
+        # 2 - script complete
+        # 3 - no more mines
+        # 4 - conditions 2 & 3
+        return end_game
+    def print_field(self):
+        """ Displays the current field """
+        for i in range(len(self.field)):
+            display = ""
+            for j in range(len(self.field[0])):
+                if self.field[i][j] == None:
+                    display += '.'
+                else:
+                    display += self.depth_to_char(self.field[i][j])
+            print display
+        print ""
+    def step(self):
+        """ Executes the actions and updates the relative field """
+        # Get and print action
+        act = self.get_action()
+        for a in act:
+            self.execute(a)
+    def get_action(self):
+        """ Gets the next action and prints """
+        act = self.actions[0]
+        self.actions = self.actions[1:] # Remove action
+        display = ""
+        for a in act:
+            display += a + " "
+        print display + '\n'
+        return act
+    def execute(self, act):
+        """ Updates the field based on the action """
+        pass
+        #if act in self.shoot.keys():
+        #if act in self.move.keys():
+
+
 
 
 
@@ -116,10 +184,11 @@ def main():
     game.setup(args.field, args.script)
     # Check for valid game
     if not game.valid():
+        print "Error: Game setup is not valid"
         sys.exit(-1)
     # else: continue
     # Play game to completion
-    #result, score = game.play()
+    result, score = game.play()
     # Print result
     #print result + "(" + str(score) + ")"
     return 0 # Successful
