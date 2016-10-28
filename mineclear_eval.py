c5ed20d2d50fd9c517d6a1d2f473299f346aeb38
Dax Garner <dax.garner@gmail.com>
2016-10-27 20:47:56 -0500

    Completed initial functioning script that passes all examples and tests a few additional input failure cases

--- mineclear_eval.py
+++ mineclear_eval.py
@@ -16,19 +16,22 @@ class Game:
         """ Initializes game with nothing to do """
         self.setup_valid = False
         self.actions = []
-        self.move = {'north': -1, 'south': 1, 'east': 1, 'west': -1}
-        self.shoot = {'alpha':[(-1, -1), (-1,  1), (1, -1), (1, 1)],
-                      'beta': [(-1,  0), ( 0, -1), (0,  1), (1, 0)],
-                      'gamma':[(-1,  0), ( 0,  0), (1,  0)],
-                      'delta':[( 0, -1), ( 0,  0), (0,  1)]
+        self.move = {'north': [0, -1], 'south': [0, 1], 'east': [1, 0], 'west': [-1, 0]}
+        self.shoot = {'alpha':[[-1, -1], [-1,  1], [1, -1], [1, 1]],
+                      'beta': [[-1,  0], [ 0, -1], [0,  1], [1, 0]],
+                      'gamma':[[-1,  0], [ 0,  0], [1,  0]],
+                      'delta':[[ 0, -1], [ 0,  0], [0,  1]]
                      }
-        self.field = [] # This is a relative field, that maintains the ship at the center
-        self.field_size = {'x': 0, 'y': 0}
-        self.ship = (0, 0)
+        self.field = []
+        self.ship = (0, 0, 0)
         self.lowercase_offset = -96 # a = 97 = 1 km
         self.uppercase_offset = -38 # A = 65 = 27 km
         self.success = False
         self.score = 0
+        self.shoot_penalty = 0
+        self.move_penalty = 0
+        self.shoot_init = 0
+        self.move_init = 0
     def setup(self, ffile, sfile):
         """ Parses the files and initialies the action list and field """
         # Parse field file and evalute depth
@@ -37,24 +40,20 @@ class Game:
             i = 0
             for l in field_lines:
                 j = 0
-                self.field.append([])
                 for c in l:
                     # Detect mine
                     if (c in string.ascii_lowercase) or (c in string.ascii_uppercase):
                         # Add to field
-                        self.field[i].append(self.char_to_depth(c))
+                        self.field.append([j, i, self.char_to_depth(c)])
                         # Update starting score
-                        self.score = self.score + 10
+                        self.score += 10
+                        self.shoot_init += 5
+                        self.move_init += 3
                     else: # No mine detected
                         if c != '.':
                             print "Warning: Character " + c + " implies no mine in field at: (" + str(i) + ", " + str(j) + ")"
-                        self.field[j].append(None)
                     j = j + 1
-                self.field_size['x'] = j if (j > self.field_size['x']) else self.field_size['x']
                 i = i + 1
-            self.field_size['y'] = i
-        # Set ship location
-        self.ship = (i/2, j/2)
         # Parse script action list
         with open(sfile) as sf:
             action_lines = sf.readlines()
@@ -65,9 +64,10 @@ class Game:
         d = 0
         if char in string.ascii_lowercase:
             d = ord(char) + self.lowercase_offset
-        elif char in string.ascii_lowercase:
+        elif char in string.ascii_uppercase:
             d = ord(char) + self.uppercase_offset
         else: # Character has no determined depth
+            "Warning: Unknown object, assumed not to be a mine: " + c
             d = 0
         return d
     def depth_to_char(self, depth):
@@ -86,36 +86,50 @@ class Game:
     def valid(self):
         """ Checks the inputs to the game are valid prior to playing """
         # Field Checks
-        # No checks: All potential field failure are gracefully interpreted
+        if not self.field:
+            print "Warning: No mines... you've already won!"
         # Script Checks
         i = 0
         for s in self.actions:
             if len(s) > 2:
                 print "Error: A maximum of two actions per step are allowed: line " + str(i)
                 return False
-            elif len(s) > 1:
-                # Check first action is type shooting
-                if s[0] not in self.shoot.keys():
-                    print "Error: First of two actions must be a firing pattern: " + s[0]
-                    return False
-                # Check second is type movement
-                if s[1] not in self.move.keys():
-                    print "Error: Second of two actions must be a movement pattern: " + s[1]
-                    return False
             elif len(s) > 0:
-                # Check action is of either type
-                if (s[0] not in self.move.keys()) and (s[0] not in self.shoot.keys()):
-                    print "Error: Unknown action: " + s[0]
+                m = 0
+                sh = 0
+                for a in s:
+                    # Check action is of either type
+                    if (a not in self.move.keys()) and (a not in self.shoot.keys()):
+                        print "Error: Unknown action: " + a
+                        return False
+                    if a in self.move.keys():
+                        m += 1
+                    if a in self.shoot.keys():
+                        sh += 1
+                if (m > 1) or (sh > 1):
+                    print "Error: Cannot shoot or move twice in one step: " + s[0] + " " + s[1]
+                    return False
             else:
                 continue
         self.setup_valid = True
         return True
+
+    def init(self):
+        # Set ship location offset and update mine locations
+        if self.field:
+            self.ship = (sum(m[0] for m in self.field)/len(self.field), sum(m[1] for m in self.field)/len(self.field))
+            for m in self.field:
+                m[0] = m[0] - self.ship[0]
+                m[1] = m[1] - self.ship[1]
+        else:  # strange case
+            self.ship = (0, 0)
 
     def play(self):
         """ Cyclical operations of the game after a valid setup """
         if not self.setup_valid:
             print "Error: Game was not properly setup"
             return "fail", 0
+        self.init()
         i = 1
         while(not self.end()):
             print "Step " + str(i) + '\n'
@@ -123,35 +137,61 @@ class Game:
             self.step()
             self.print_field()
             i = i + 1
-            sys.exit()
         result = "pass" if self.success else "fail"
         return result, self.score
 
     def end(self):
-        """ Checks for all end conditions """
-        end_game = False
-        # 1 - passed mine
+        """ Checks for all end conditions and sets score """
         # 2 - script complete
+        if not self.actions:
+            self.success = True
+            if self.field:
+                self.success = False
+                self.score = 0
+            return True
         # 3 - no more mines
-        # 4 - conditions 2 & 3
-        return end_game
+        if not self.field:
+            self.success = True
+            if self.actions:
+                self.success = True
+                self.score = 1
+            return True
+        # 1 - passed mine
+        if min(m[2] for m in self.field) <= 0:
+            self.success = False
+            self.score = 0
+            return True
+        # 4 - conditions 2 & 3, already checked
+        return False
+
     def print_field(self):
         """ Displays the current field """
-        for i in range(len(self.field)):
+        if not self.field: # base case
+            print '.\n'
+            return
+        # else: figure it out
+        # Get max abs of x, y axis
+        x = max(abs(m[0]) for m in self.field)
+        y = max(abs(m[1]) for m in self.field)
+        mxy = [[m[0], m[1]] for m in self.field]
+        for j in range(-y, y + 1):
             display = ""
-            for j in range(len(self.field[0])):
-                if self.field[i][j] == None:
-                    display += '.'
+            for i in range(-x, x + 1):
+                index = [idx for idx, xy in enumerate(mxy) if xy == [i, j]]
+                if index:
+                    display += self.depth_to_char(self.field[index[0]][2])
                 else:
-                    display += self.depth_to_char(self.field[i][j])
+                    display += '.'
             print display
         print ""
+
     def step(self):
         """ Executes the actions and updates the relative field """
         # Get and print action
         act = self.get_action()
         for a in act:
             self.execute(a)
+        self.drop()
     def get_action(self):
         """ Gets the next action and prints """
         act = self.actions[0]
@@ -159,19 +199,46 @@ class Game:
         display = ""
         for a in act:
             display += a + " "
+        if not act:
+            display = "no action"
         print display + '\n'
         return act
     def execute(self, act):
         """ Updates the field based on the action """
-        pass
-        #if act in self.shoot.keys():
-        #if act in self.move.keys():
-
-
-
-
-
-
+        mxy = [[m[0], m[1]] for m in self.field]
+        if act in self.shoot.keys():
+            for t in self.shoot[act]:
+                index = [idx for idx, xy in enumerate(mxy) if xy == t]
+                if index:
+                    del self.field[index[0]]
+                # else: continue
+            # Update Score
+            if self.shoot_penalty <= self.shoot_init:
+                if (self.shoot_penalty + 5) <= self.shoot_init:
+                    self.shoot_penalty += 5
+                    self.score -= 5
+                else:
+                    self.score -= self.shoot_init - self.shoot_penalty
+                    self.shoot_penalty = self.shoot_init
+        elif act in self.move.keys():
+            # Move ship
+            move = self.move[act]
+            for m in self.field:
+                m[0] -= move[0]
+                m[1] -= move[1]
+            # Update Score
+            if self.move_penalty <= self.move_init:
+                if (self.move_penalty + 2) <= self.move_init:
+                    self.move_penalty += 2
+                    self.score -= 2
+                else:
+                    self.score -= self.move_init - self.move_penalty
+                    self.move_penalty = self.move_init
+        else: # do nothing
+            pass
+    def drop(self):
+        for m in self.field:
+            m[2] -= 1
 
 def main():
     """ Top-level executing function """
@@ -190,7 +257,7 @@ def main():
     # Play game to completion
     result, score = game.play()
     # Print result
-    #print result + "(" + str(score) + ")"
+    print result + " (" + str(score) + ")"
     return 0 # Successful
 
 def parse_user_args() :
